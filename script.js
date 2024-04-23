1676e2fc725d59356c3d535c6794584d374a4ec7
Hairu Firmandi <firmandi05@gmail.com>
2024-04-23 20:04:14 +0700

    add subtract function and remove add function initial value

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function add(...nums) {
   const result = nums.reduce((acc, num) => {
     return acc + num
-  }, 0)
+  })
   return result
 }
+
+function subtract(...nums) {
+  const result = nums.reduce((acc, num) => {
+    return acc - num
+  })
+  return result
+}
+
