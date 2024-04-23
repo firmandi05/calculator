e6a7ed05f43d5452c317a7aa6793fd40fddd72ef
Hairu Firmandi <firmandi05@gmail.com>
2024-04-23 20:17:18 +0700

    add operate function
    doing things based on calculation that user wanted

--- script.js
+++ script.js
@@ -30,3 +30,15 @@ function divide(...nums) {
   return result
 }
 
+function operate(num1, num2, operator) {
+  switch(operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case 'x':
+      return multiply (num1,num2);
+    case '/':
+      return divide(num1, num2);
+  };
+};
